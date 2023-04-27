d985e130ddb0c002acc1dd39ffaf1eebce6c5b66
Parisa <131019878+parisahmz@users.noreply.github.com>
2023-04-27 18:11:05 +0330

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,18 @@
-//Tamrin1&2-3
+//Tamrin1&2>>number2//
 package com.company;
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-
-            System.out.println("Enter Length Of Yard:");
-            float length = input.nextFloat();
-
-            System.out.println("Enter Width Of Yard:");
-            float width = input.nextFloat();
-
-            float areay = width * length;
-            System.out.println("Area of your yard is:" + areay);
-
-            System.out.println("Enter Length Of House:");
-            float x = input.nextFloat();
-
-            System.out.println("Enter Width Of House:");
-            float y = input.nextFloat();
-
-            float areah = x * y;
-            System.out.println("Area of your house is:" + areah);
-
-            float delta = areay - areah;
-            System.out.println("Delta area is " + delta);
-
-            System.out.println("The Average speed of Lawn Mower is " + 2.3);
-            float t = ( ( (10 * delta) / 23) / 60 );
-            System.out.println("The Time Of Lawn Mower Working Is: " + t + "min");
-        }
+        System.out.println("ADD TOW INTEGER NUMBER TO CALCULATE : ");
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        float taqsim = num1 / num2;
+        System.out.println(taqsim);
     }
-
+}
