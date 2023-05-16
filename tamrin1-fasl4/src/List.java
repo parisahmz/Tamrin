f37374e9b7ee8cc4b95d7dc78b2b84fcca199978
Parisa <131019878+parisahmz@users.noreply.github.com>
2023-05-16 20:04:34 +0330

    Add files via upload

---
+++ tamrin1-fasl4/src/List.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class List {
+
+	public static void main( String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String[] strArray = new String [4];
+		System.out.println("Type 4 words: ");
+		for (int i=0 ; i<=3; i++) {
+			strArray[i] = scanner.nextLine();
+			}
+		
+		Arrays.sort(strArray);
+		System.out.println("words : ");
+	    for (int j=0; j<strArray.length ; j++) {
+	      System.out.print(strArray[j] + " , ");
+	      }
+	    }
+	}
